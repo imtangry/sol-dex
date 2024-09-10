163350649b124bbaad40927fda7936e6508a82d4
不知名的底层码农T先生 <tang.rui.yu@foxmail.com>
2024-09-10 16:06:39 +0800

    feat: 连接按钮添加多语言支持

--- src/components/navbar/ConnectWallet.tsx
+++ src/components/navbar/ConnectWallet.tsx
@@ -1,8 +1,21 @@
 'use client'
 
-import {WalletMultiButton} from '@solana/wallet-adapter-react-ui'
-import { Lang } from '~/i18n/config'
+import {BaseWalletMultiButton} from '@solana/wallet-adapter-react-ui'
+import React from 'react'
+import { useTranslation } from '~/i18n/client'
+
+import {Lang} from '~/i18n/config'
 
 export default function ConnectWallet({lang}: {lang: Lang}) {
-  return <WalletMultiButton style={{}} />
+  const {t} = useTranslation(lang)
+  const LABELS = {
+    'change-wallet': t('changeWallet'),
+    connecting: t('connecting'),
+    'copy-address': t('copyAddress'),
+    copied: t('copied'),
+    disconnect: t('disconnect'),
+    'has-wallet': t('hasWallet'),
+    'no-wallet': t('noWallet'),
+  } as const
+  return <BaseWalletMultiButton labels={LABELS} />
 }
