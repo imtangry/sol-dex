f97d34274f12ff2b5a043f71c85bf98b3f400725
Rhys <tang.rui.yu@foxmail.com>
2024-09-14 16:15:39 +0800

    feat: 添加loading

--- src/components/swap/Swap.tsx
+++ src/components/swap/Swap.tsx
@@ -11,11 +11,13 @@ import {useEffect, useState} from 'react'
 import ConnectWallet from '../navbar/ConnectWallet'
 import {SwapAsset, SwapCard} from './SwapCard'
 import {Lang} from "~/i18n/config";
+import {Skeleton} from "@/components/ui/skeleton";
 
 const cardHeight = 160
 let tokens:SwapAsset[] = []
 export const Swap = ({lang}:{lang:Lang}) => {
   const {publicKey} = useWallet()
+  const [loading, setLoading] = useState<boolean>(true)
   const [tokenList, setTokenList] = useState(tokens)
 
   const [send, setSend] = useState<SwapAsset>({} as SwapAsset)
@@ -36,6 +38,7 @@ export const Swap = ({lang}:{lang:Lang}) => {
       setTokenList(data)
       setSend({...data[0],value:0})
       setReceive({...data[1],value:0})
+      setLoading(false)
     }
     if(tokens.length=== 0){
       getTokens()
@@ -44,22 +47,29 @@ export const Swap = ({lang}:{lang:Lang}) => {
   return (
     <div className='page-swap animate-in slide-in-from-bottom space-y-4'>
       <div className='relative space-y-2'>
-        <SwapCard
-          type='send'
-          asset={send}
-          setAsset={setSend}
-          height={cardHeight}
-          tokens={tokenList}
+        {loading? <>
+          <div className='space-y-2 rounded-lg bg-neutral-200 px-4 py-2'>
+            <Skeleton className="h-[70px] w-full" />
+            <Skeleton className="h-[70px] w-full" />
+          </div>
+          <div className='space-y-2 rounded-lg bg-neutral-200 px-4 py-2'>
+            <Skeleton className="h-[70px] w-full" />
+            <Skeleton className="h-[70px] w-full" />
+          </div>
+        </> : <><SwapCard
+            type='send'
+            asset={send}
+            setAsset={setSend}
+            height={cardHeight}
+            tokens={tokenList}
         />
-
-        <SwapCard
-          type='receive'
-          asset={receive}
-          setAsset={setReceive}
-          height={cardHeight}
-          tokens={tokenList}
-        />
-
+          <SwapCard
+              type='receive'
+              asset={receive}
+              setAsset={setReceive}
+              height={cardHeight}
+              tokens={tokenList}
+          /></>}
         <Button
           size='rounded'
           className='absolute left-1/2 h-[62px] w-[62px] -translate-x-1/2 -translate-y-1/2 bg-[#512da8] hover:bg-[#000]'
